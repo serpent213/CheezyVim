a55863a32a740c03543b00dcd80937a7326a86ed
Steffen Beyer <steffen@beyer.io>
2025-04-04 12:44:57 +0200

    Flake update

--- config/keymaps/keymaps-plugins.nix
+++ config/keymaps/keymaps-plugins.nix
@@ -199,5 +199,37 @@
         desc = "Toggle treeSJ";
       };
     }
+
+    # Harpoon
+    {
+      mode = "n";
+      key = "hx";
+      action.__raw = "function() require'harpoon':list():add() end";
+    }
+    {
+      mode = "n";
+      key = "hm";
+      action.__raw = "function() require'harpoon'.ui:toggle_quick_menu(require'harpoon':list()) end";
+    }
+    {
+      mode = "n";
+      key = "<C-j>";
+      action.__raw = "function() require'harpoon':list():select(1) end";
+    }
+    {
+      mode = "n";
+      key = "<C-k>";
+      action.__raw = "function() require'harpoon':list():select(2) end";
+    }
+    {
+      mode = "n";
+      key = "<C-l>";
+      action.__raw = "function() require'harpoon':list():select(3) end";
+    }
+    {
+      mode = "n";
+      key = "<C-m>";
+      action.__raw = "function() require'harpoon':list():select(4) end";
+    }
   ];
 }
