2794cd14cf05b31ed206da2724962362bf209b7c
Steffen Beyer <steffen@beyer.io>
2025-06-21 16:32:51 +0200

    refactor: Modernise Avante config

--- config/ai/avante.nix
+++ config/ai/avante.nix
@@ -3,19 +3,30 @@
     avante = {
       enable = mkDefault true;
       settings = {
-        provider = "openai";
-        openai = {
-          endpoint = "https://api.openai.com/v1";
-          max_tokens = 4096;
-          model = "o3-mini";
-          temperature = 0;
+        provider = "gemini";
+        providers = {
+          gemini = {
+            model = "gemini-2.5-flash";
+            temperature = 0;
+            max_tokens = 100000;
+          };
+          openai = {
+            endpoint = "https://api.openai.com/v1";
+            model = "gpt-4o";
+            extra_request_body = {
+              temperature = 0; # Lower temperature for more deterministic output
+              max_tokens = 100000; # Max tokens for the response
+            };
+          };
+          claude = {
+            endpoint = "https://api.anthropic.com";
+            model = "claude-4-0-sonnet-latest";
+            extra_request_body = {
+              temperature = 0; # Lower temperature for more deterministic output
+              max_tokens = 100000; # Max tokens for the response (Claude 3.5 Sonnet supports up to 200k tokens)
+            };
+          };
         };
-        # claude = {
-        #   endpoint = "https://api.anthropic.com";
-        #   max_tokens = 4096;
-        #   model = "claude-3-5-sonnet-latest";
-        #   temperature = 0;
-        # };
         diff = {
           autojump = true;
           debug = false;
@@ -33,11 +44,11 @@
         behaviour = {
           auto_suggestions = false; # Experimental stage
           # auto_set_highlight_group = true;
-          # auto_set_keymaps = true;
+          # auto_set_keymaps = true; # Automatically set keymaps for convenience
           # auto_apply_diff_after_generation = false;
           # support_paste_from_clipboard = false;
-          # minimize_diff = true; # Whether to remove unchanged lines when applying a code block
-          # enable_token_counting = true; # Whether to enable token counting. Default to true.
+          minimize_diff = true; # Whether to remove unchanged lines when applying a code block
+          enable_token_counting = true; # Whether to enable token counting. Default to true.
         };
         mappings = {
           diff = {
