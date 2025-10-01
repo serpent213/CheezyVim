f5bc57fe8b932259c883eedab13782a54fac5f43
Steffen Beyer <steffen@beyer.io>
2025-10-01 21:20:12 +0200

    chore: Migrate lspsaga settings

--- config/lsp/lspsaga.nix
+++ config/lsp/lspsaga.nix
@@ -1,19 +1,23 @@
 {pkgs, ...}: {
   plugins.lspsaga = {
     enable = pkgs.lib.mkDefault true;
-    ui.border = "rounded";
-    ui.codeAction = "";
-    beacon.enable = pkgs.lib.mkDefault true;
-    implement = {
-      enable = pkgs.lib.mkDefault true;
-      sign = pkgs.lib.mkDefault false;
+    settings = {
+      ui.border = "rounded";
+      ui.codeAction = "";
+      beacon.enable = pkgs.lib.mkDefault true;
+      implement = {
+        enable = pkgs.lib.mkDefault true;
+        sign = pkgs.lib.mkDefault false;
+      };
+      lightbulb = {
+        enable = pkgs.lib.mkDefault true;
+        sign = pkgs.lib.mkDefault true;
+        virtualText = pkgs.lib.mkDefault false;
+      };
+      outline = {
+        layout = "float";
+      };
+      symbolInWinbar.enable = pkgs.lib.mkDefault true;
     };
-    lightbulb = {
-      enable = pkgs.lib.mkDefault true;
-      sign = pkgs.lib.mkDefault true;
-      virtualText = pkgs.lib.mkDefault false;
-    };
-    outline = {layout = "float";};
-    symbolInWinbar.enable = pkgs.lib.mkDefault true;
   };
 }
